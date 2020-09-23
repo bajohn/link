1295528f262f3a19aaf30b63eff2321044dbbb3a
Brendan John <bjohn454@gmail.com>
2020-09-22 20:58:34 -0400

    metamask connection successful in ng

---
+++ frontend/src/app/WEB3.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Inject, InjectionToken } from '@angular/core';
+
+import Web3 from 'web3';
+
+`
+- Create a singleton Web3 provider
+- Provide at root of this app
+- Hit .enable(), which requests the user's approval to link Metamask with this app.
+`
+
+export const WEB3 = new InjectionToken<Web3>('web3', {
+  providedIn: 'root',
+  factory: () => {
+    try {
+      const provider = ('ethereum' in window) ? window['ethereum'] : Web3.givenProvider;
+      console.log('provider');
+      console.log(provider);
+      provider.enable();
+      return new Web3(provider);
+    } catch (err) {
+      throw new Error('Non-Ethereum browser detected. You should consider trying Mist or MetaMask!');
+    }
+  }
+});
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
+import Web3 from 'web3'
+import { WEB3 } from './WEB3'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(@Inject(WEB3) private web3: Web3) { }
+
+  async ngOnInit() {
+    const x = await this.getAccounts();
+    console.log(x);
+  }
+
+  private async getAccounts() {
+    this.web3.eth.getAccounts(console.log)
+    const balance = await this.web3.eth.getBalance('0x068528704bAFD8A4B42985Baf87b8877fBea2E35');
+    console.log(balance);
+    return balance
+  }
+
 }
+
